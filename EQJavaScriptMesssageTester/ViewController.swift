5932999aa9d4fe6b816b797c08aed0dc6c93d20c
Helmut Pfister <pfister@softabout.de>
2018-11-11 00:17:19 +0100

    EQSTAR-Test JavaScript postMessage

--- EQJavaScriptMesssageTester/ViewController.swift
+++ EQJavaScriptMesssageTester/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        guard let serverTrust = challenge.protectionSpace.serverTrust else { return completionHandler(.useCredential, nil) }
+        let exceptions = SecTrustCopyExceptions(serverTrust)
+        SecTrustSetExceptions(serverTrust, exceptions)
+        completionHandler(.useCredential, URLCredential(trust: serverTrust))
     }
-
-
+    
+    override func viewDidLoad() {
+        
+    
+    super.viewDidLoad()
+     
+        let config = WKWebViewConfiguration()
+        let contentController = WKUserContentController()
+        config.userContentController = contentController
+        let scriptSource = "var messageData = {field1: 'Hello', field2: 'World'}; window.postMessage(messageData, '*');"
+        
+        let userScript = WKUserScript(source: scriptSource, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        contentController.addUserScript(userScript)
+        let webView = WKWebView(frame: .zero, configuration: config)
+        
+        view.addSubview(webView)
+        let layoutGuide = view.safeAreaLayoutGuide
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor).isActive = true
+        webView.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor).isActive = true
+        webView.topAnchor.constraint(equalTo: layoutGuide.topAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor).isActive = true
+        
+        if let url = URL(string: "https://eq-preorder-dev.mercedes-benz.com/public/eqready.html") {
+            webView.load(URLRequest(url: url))
+        }
 }
 
+}
