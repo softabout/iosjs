ad4a643d20d61610a5c210baa591910bef443885
Helmut Pfister <pfister@softabout.de>
2018-11-10 21:12:00 +0100

    Initial Commit

---
+++ EQJavaScriptMesssageTester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EQJavaScriptMesssageTester
+//
+//  Created by Helmut Pfister on 10.11.18.
+//  Copyright © 2018 softabout.de. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
